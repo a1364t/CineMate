63539d762b533f48b4474451084e02d61e3c37d6
Alireza <ali1985reza@gmail.com>
2024-11-07 15:44:12 +1100

    add card component

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,3 +1,16 @@
+import { Card } from "../components";
 export const MovieList = () => {
-  return <main>MoviList</main>;
+  return (
+    <main>
+      <section>
+        <div className="flex justify-start flex-wrap">
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+        </div>
+      </section>
+    </main>
+  );
 };
